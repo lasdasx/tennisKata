188e8d45efe26950a3ff38dc644aca7bd80383ab
lasdasx <hristoslasdas@gmail.com>
2026-05-10 17:46:03 +0300

    setup

---
+++ TennisKata.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace TennisKata.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void New_game_Score()
+    {
+        var game = new TennisGame();
+
+        var result = game.GetScore();
+
+        Assert.Equal("Love All", result);
+    }
+}
